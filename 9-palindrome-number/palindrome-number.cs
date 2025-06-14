372206db533c21a350f3a625157ad8ffde189bc1
Hager-A <ha1316625@gmail.com>
2025-06-14 16:23:20 +0300

    Time: 15 ms (12.19%) | Memory: 40.6 MB (6.90%) - LeetSync

--- 9-palindrome-number/palindrome-number.cs
+++ 9-palindrome-number/palindrome-number.cs
@@ -1,6 +1,12 @@
 public class Solution {
     public bool IsPalindrome(int x) {
-        string y = new string(x.ToString().Reverse().ToArray());
-        return y==x.ToString();
+        if (x < 0)
+        {
+            return false;
+        }
+        else {
+            string y = new string(x.ToString().Reverse().ToArray());
+            return y == x.ToString();
+  }
     }
 }
