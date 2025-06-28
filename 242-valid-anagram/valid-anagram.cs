2d92239836292d16e6cf85008c303b9fdc913b1b
Hager-A <ha1316625@gmail.com>
2025-06-28 03:23:26 +0300

    Time: 49 ms (9.74%) | Memory: 46.7 MB (5.33%) - LeetSync

--- 242-valid-anagram/valid-anagram.cs
+++ 242-valid-anagram/valid-anagram.cs
@@ -1,5 +1,8 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
+         if (s.Length != t.Length)
+        return false;
+
         s = new string(s.ToCharArray().OrderBy(c => c).ToArray());
 t = new string(t.ToCharArray().OrderBy(c => c).ToArray());
 if (s == t)
