2b089ebc4589a29e26edfe24585a228cacd35943
Hager-A <ha1316625@gmail.com>
2025-07-02 17:32:42 +0300

    Time: 138 ms (74.51%) | Memory: 53.2 MB (24.25%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+    if (nums.Length <= 2) return nums.Length;
+
+    int index = 2;
+
+    for (int i = 2; i < nums.Length; i++)
+    {
+        if (nums[i] != nums[index - 2])
+        {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+
+    return index;
+
+    }
+}
